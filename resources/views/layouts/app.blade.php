46fa9e53842239e4a06c857f3b4fe2e4201ae493
prakash <pcmishra22@gmail.com>
2026-05-21 08:23:57 +0530

    some fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,9 +12,9 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link rel="shortcut icon" href="{{ asset('backend/img/icons/icon-48x48.png') }}" />
 
-	<link rel="canonical" href="https://demo-basic.adminkit.io/" />
+	<link rel="canonical" href="{{ url()->current() }}" />
 
-	<title>AdminKit Demo - Bootstrap 5 Admin Template</title>
+	<title>@yield('title', 'IndianestHub - Real Estate Portal')</title>
 
 	<link href="{{ asset('backend/css/app.css') }}" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
